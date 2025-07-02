a1befc2ffc6a1410c46fde1acb3f8fa71cfe0c0d
mohammedmatheen <mohammed.matheen@nirogstreet.in>
2025-07-02 12:11:03 +0530

    github web test fix

--- naya-web/src/App.test.js
+++ naya-web/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginBtn = screen.getByRole('button', { name: /login/i });
+  expect(loginBtn).toBeTruthy();
 });
